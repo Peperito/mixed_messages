767e459cd82840ae68b8211a189a11a1e0262823
Vince P <32515747+Peperito@users.noreply.github.com>
2022-05-16 23:34:26 +0200

    Base code to improve perfect design and pictures

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 Short program to return a random piece of wisdom and advice on a user input.
 Users have to enter a valid star sign.
 Goal of this project is to study proper project structure and project implementation via Git / GitHub
+
+---------------
+
+The first commit is an ugly website that only works for Aquarius and Capricorn and returns a basic alert box.  
+
+This will serve to implement new features I learn during my lessons
---
+++ main.js
@@ -0,0 +1,21 @@
+// Create a random number to select the message to display among 6 messages
+
+const getRandomNum = () => {
+    return Math.floor(Math.random()*6);
+}
+
+//return a random prediction among according to the star sign, first build only for Capricorn and Aquarius
+const createPrediction = () => {
+    var starSign = window.prompt("What is your star sign?")
+    const predictions = ["Tu vas trouver l'amour", "Tu sens mauvais", "Tu vas manger des fraises", "Tu vas gagner beaucoup d'argent", "Tu vas faire caca", "Prout"]
+    let rand = getRandomNum()
+    switch(starSign){
+        case "Capricorn":
+        return window.alert(predictions[rand])
+        break;
+        case "Aquarius":
+        return window.alert(predictions[rand])
+        break;
+        default: return console.log("Not a valid star sign")
+    }
+}
