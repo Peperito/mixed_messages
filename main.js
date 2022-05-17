8582fe2cbe17b63d0614f7ed348726611783f8b6
Vince P <32515747+Peperito@users.noreply.github.com>
2022-05-17 12:37:09 +0200

    uograded visuals, prediction displays directly on the page

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ const getRandomNum = () => {
 //return a random prediction among according to the star sign, first build only for Capricorn and Aquarius
 const createPrediction = () => {
     var starSign = window.prompt("What is your star sign?")
-    const predictions = ["Tu vas trouver l'amour", "Tu sens mauvais", "Tu vas manger des fraises", "Tu vas gagner beaucoup d'argent", "Tu vas faire caca", "Prout"]
+    const predictions = ["Tu vas trouver l'amour", "Tu sens mauvais", "Tu vas manger des fraises", "Tu vas gagner beaucoup d'argent", "Tu vas faire caca", "Prout!"]
     let rand = getRandomNum()
     switch(starSign){
         case "Capricorn":
-        return window.alert(predictions[rand])
+        return document.getElementById('answer').innerHTML = predictions[rand];
         break;
         case "Aquarius":
-        return window.alert(predictions[rand])
+        return document.getElementById('answer').innerHTML = predictions[rand];
         break;
         default: return console.log("Not a valid star sign")
     }
